7e5a6ac11b3d702e61f3a2aeec7744d98f5cceb2
Jer403 <jeremiwtfxd@gmail.com>
2025-03-26 16:38:10 -0400

    Updated details

--- src/components/DropDownTab.tsx
+++ src/components/DropDownTab.tsx
@@ -18,15 +18,11 @@ export function DropDownTab({
       className={`w-full flex ${
         currentQuestion == id ? "gap-2 " : "gap-0"
       } flex-col p-3 border cursor-pointer border-[--border_light_300] rounded-lg`}
+      onClick={() =>
+        currentQuestion == id ? setCurrentQuestion("") : setCurrentQuestion(id)
+      }
     >
-      <div
-        className="w-full flex justify-between items-center gap-1"
-        onClick={() =>
-          currentQuestion == id
-            ? setCurrentQuestion("")
-            : setCurrentQuestion(id)
-        }
-      >
+      <div className="w-full flex justify-between items-center gap-1">
         <p className="text-md">{question}</p>
         {currentQuestion == id ? (
           <ChevronUp className="text-[--text_light_200]" />
