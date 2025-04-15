b512e3791c9599dd35e57aa8cf83f7e266188a45
Jer403 <jeremiwtfxd@gmail.com>
2025-04-14 20:03:05 -0400

    Debugging

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -2,8 +2,7 @@ import { useEffect } from "react";
 
 export default function Redirect() {
   useEffect(() => {
-    // location.href =
-    //   "https://modelfantasy.up.railway.app/app/auth/google/callback";
+    location.href = `https://modelfantasy.up.railway.app/app/auth/google/callback${window.location.search}`;
   }, []);
   return <></>;
 }
