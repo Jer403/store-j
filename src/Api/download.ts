d4cc3d1562986a6638da9c8ac40c0e450d61357d
Jer403 <jeremiwtfxd@gmail.com>
2025-04-15 20:28:26 -0400

    Debugging

--- src/Api/download.ts
+++ src/Api/download.ts
@@ -2,7 +2,12 @@ import axios from "./axios.ts";
 
 export const download = async (id: string) => {
   try {
-    return await axios.get(`/download/${id}`);
+    return await axios.get(`/download/${id}`, {
+      onUploadProgress: (progressEvent) => {
+        const loaded = progressEvent.loaded;
+        console.log(loaded);
+      },
+    });
   } catch (error) {
     console.log(error);
   }
